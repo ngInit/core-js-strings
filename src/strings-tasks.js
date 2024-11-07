930ed389627c41376442075f4d0dff6cede4a105
Aleksei Nikolaev <aleksey.nikolaev.eng@gmail.com>
2024-11-07 10:59:23 +0100

    feat: solve removeLastOccurrences

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -165,8 +165,11 @@ function removeFirstOccurrences(str, value) {
  *   removeLastOccurrences('I like legends', 'end') => 'I like legs'.
  *   removeLastOccurrences('ABABAB', 'BA') => 'ABAB'.
  */
-function removeLastOccurrences(/* str, value */) {
-  throw new Error('Not implemented');
+function removeLastOccurrences(str, value) {
+  const valueIndex = str.lastIndexOf(value);
+  return valueIndex !== -1
+    ? str.slice(0, valueIndex) + str.slice(valueIndex + value.length)
+    : str;
 }
 
 /**
