3c86edb8404445a5d52c926ef2ed8d2764fcdd19
Aleksei Nikolaev <aleksey.nikolaev.eng@gmail.com>
2024-11-07 10:56:20 +0100

    feat: solve removeFirstOccurrences

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -146,8 +146,11 @@ function repeatString(str, times) {
  *   removeFirstOccurrences('I like legends', 'end') => 'I like legs'.
  *   removeFirstOccurrences('ABABAB', 'BA') => 'ABAB'.
  */
-function removeFirstOccurrences(/* str, value */) {
-  throw new Error('Not implemented');
+function removeFirstOccurrences(str, value) {
+  const valueIndex = str.indexOf(value);
+  return valueIndex !== -1
+    ? str.slice(0, valueIndex) + str.slice(valueIndex + value.length)
+    : str;
 }
 
 /**
