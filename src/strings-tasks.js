32d2ab3ab4c1776208529482812e9c10e1db9c5e
Aleksei Nikolaev <aleksey.nikolaev.eng@gmail.com>
2024-11-07 13:54:30 +0100

    feat: solve invertCase

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -385,8 +385,12 @@ function reverseWords(str) {
  *   invertCase('JavaScript is Fun') => 'jAVAsCRIPT IS fUN'
  *   invertCase('12345') => '12345'
  */
-function invertCase(/* str */) {
-  throw new Error('Not implemented');
+function invertCase(str) {
+  return str.replace(/./g, (symbol) =>
+    symbol === symbol.toUpperCase()
+      ? symbol.toLowerCase()
+      : symbol.toUpperCase()
+  );
 }
 
 /**
