c73081ef03334d23aca71a2685e8793594b923ae
Aleksei Nikolaev <aleksey.nikolaev.eng@gmail.com>
2024-11-07 13:04:37 +0100

    feat: solve isPalindrome

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -326,8 +326,10 @@ function countVowels(str) {
  *   isPalindrome('apple') => false
  *   isPalindrome('No lemon, no melon') => true
  */
-function isPalindrome(/* str */) {
-  throw new Error('Not implemented');
+function isPalindrome(str) {
+  const compressedStr = str.toLowerCase().replace(/\W/g, '');
+  const reversedStr = compressedStr.split('').reverse().join('');
+  return Boolean(compressedStr.localeCompare(reversedStr)) !== true;
 }
 
 /**
