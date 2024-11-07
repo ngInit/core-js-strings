54ed909f26390301ae3da7e52f387707dd08d1b4
Aleksei Nikolaev <aleksey.nikolaev.eng@gmail.com>
2024-11-07 13:50:08 +0100

    feat: solve reverseWords

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -366,8 +366,12 @@ function findLongestWord(sentence) {
  *   reverseWords('Hello World') => 'olleH dlroW'
  *   reverseWords('The Quick Brown Fox') => 'ehT kciuQ nworB xoF'
  */
-function reverseWords(/* str */) {
-  throw new Error('Not implemented');
+function reverseWords(str) {
+  const arrayOfWords = str.split(' ');
+  for (let i = 0; i < arrayOfWords.length; i += 1) {
+    arrayOfWords[i] = arrayOfWords[i].split('').reverse().join('');
+  }
+  return arrayOfWords.join(' ');
 }
 
 /**
